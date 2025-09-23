0fa98d196d58060c5caec635d16ffa3ccc8b7aed
Muksith <Mohamedmukkicolombo@gmail.com>
2025-09-23 23:08:56 +0530

    Added: changes on root directory

--- config/config.py
+++ config/config.py
@@ -1,9 +1,10 @@
 import os
-
+from pathlib import Path
 
 def setup():
     username = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
     dsn = os.getenv("DB_DSN")
-    wallet_dir = os.getenv("DB_WALLET_DIR")
+    root_dir = Path(__file__).resolve().parent.parent
+    wallet_dir = str(root_dir / "Resources/Wallet_FREEPDB1")
     return username, password, dsn, wallet_dir

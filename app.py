9fc358e8477c91d7ee9e30c83284b37053e475c9
Muksith <Mohamedmukkicolombo@gmail.com>
2025-09-27 10:42:11 +0530

    Added: CORS fixes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from controllers.job_controller import job_controller
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:4200'])
 
 app.register_blueprint(job_controller)
 
